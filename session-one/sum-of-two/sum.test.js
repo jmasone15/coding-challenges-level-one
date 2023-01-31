f7444af1bfa50bcfb4cc987891e839b8bb0ee571
Jordan Masone <jordan.masone@gfs.com>
2023-01-31 09:06:32 -0500

    Added jordan solved version.

--- session-one/sum-of-two/sum.test.js
+++ session-one/sum-of-two/sum.test.js
@@ -1,5 +1,9 @@
 const sum = require("./sum");
 
+test("adds 1 + 1 to equal 2", () => {
+    expect(sum(1, 1)).toBe(2);
+});
+
 test("adds 2 + 3 to equal 5", () => {
     expect(sum(2, 3)).toBe(5);
 });
